c7b565ac90fea301a508f8ac3f7e42f94eae2eb5
Clayton Surgeon <claytonsurgeon@gmail.com>
2022-01-16 21:12:18 -0800

    welch method

--- 1_glider_spec copy.py
+++ 1_glider_spec copy.py
@@ -21,7 +21,7 @@ Mock Wave
 
 
 duration = 20*60    # 20 minute sampling period
-samples  = 2**18     # number of samples in data set
+samples  = 2**10     # number of samples in data set
 
 # array of time increments
 time = np.linspace(0, duration, samples, endpoint=False)
@@ -29,40 +29,20 @@ time = np.linspace(0, duration, samples, endpoint=False)
 
 # a composite wave for basic testing
 # let's pretend this is eastward acceleration data
-# wave = 10*np.sin(2*np.pi*time * (1/100)) + 2*np.cos(2*np.pi*time * (1/200))
-wave = 10*np.sin(2*np.pi*time * (1/1)) + 2*np.cos(2*np.pi*time * (1/30))
+wave = 10*np.sin(2*np.pi*time * (1/100)) + 2*np.cos(2*np.pi*time * (1/200))
 
 
-num_of_windows = samples // 2**17
-M = samples // num_of_windows
-# M = samples // 4
+M = 2**7
+num_of_windows = samples // M - 1
 print(f"num_of_windows {num_of_windows}")
-hann_window = 0.5*(1 - np.cos(2*np.pi*np.array(range(0, M)) / (M - 0)))
-# for i in hann_window:
-#     print(i, '\n')
-window1 = wave[0:M] #* hann_window
+hann_window = 0.5*(1 - np.cos(2*np.pi*time[0:M] / (M - 0)))
+window1 = wave[0:M] * hann_window
 
-
-
-
-# plt.figure(1)
-# plt.plot(range(0, hann_window.size), hann_window)
-# # plt.xlim(0, duration)
-# plt.xlabel("time (second)")
-# # plt.ylabel("m/s^2")
-# plt.title('Original Signal in Time Domain')
-
-# plt.show()
-
-
-
-
-# exit(0)
 windows = [window1]
 counter = 0
-while counter < num_of_windows -1:
+while counter < num_of_windows:
 	# print(counter*M+(M//2), counter*M+(M//2)+M)
-	next_window = wave[counter*M+(M//2):counter*M+(M//2)+M] #* hann_window
+	next_window = wave[counter*M+(M//2):counter*M+(M//2)+M] * hann_window
 	windows.append(next_window)
 	counter += 1 
 print(len(windows))
@@ -98,35 +78,9 @@ FFT
 
 """
 
-# print(np.sum([[1,2], [2,4]]))
 
-# exit(0)
-
-# freq_space = np.fft.fftfreq(n=samples, d=sampling_freq)[0:samples//2]
-freq_space_window = np.fft.fftfreq(n=M, d=sampling_freq)[0:M//2]
-
-spectrums = []
-for window in windows:
-	# spectrums.append(np.abs(np.fft.rfft(window)[0:-1]) / (sampling_freq*M))
-	# spectrum = np.abs(np.fft.rfft(window)[0:-1]) / (sampling_freq*M)
-	spectrum = np.abs(np.fft.rfft(window)[0:-1]) / (M/2)
-	print(len(window), len(spectrum))
-	# exit(0)
-	spectrums.append(np.array(getDS(spectrum, freq_space_window)))
-
-# final_thing = np.sum(xs) / num_of_windows
-temp = spectrums[0]
-for spectrum in spectrums[1:]: 
-	temp += spectrum
-	# temp = np.add(temp, window) 
-final_thing = temp/num_of_windows
-# final_thing = [x/num_of_windows for x in temp]
-    
-
-    
 # get the magnitudee of the spectrum then normalize by number of 'buckets' in the spectrum
-# spectrum = np.abs(np.fft.rfft(wave)[0:-1]) / (sampling_freq*samples)
-spectrum = np.abs(np.fft.rfft(wave)[0:-1]) / (samples/2)
+spectrum = np.abs(np.fft.rfft(wave)[0:-1]) / (sampling_freq*samples)
 # spectrum = np.abs(np.fft.fft(wave)) / (samples/2)
 
 # an array of frequency increments
@@ -166,12 +120,6 @@ for i in range(len(displacement)):
 
 plt.figure(3)
 plt.plot(freq_space, displacement)
-plt.ylabel("Amplitude, m^2/Hz")
-plt.xlabel("freq (Hz)")
-plt.title('freq Domain')
-
-plt.figure(4)
-plt.plot(freq_space_window, final_thing)
 plt.ylabel("Amplitude, m^2/Hz")
 plt.xlabel("freq (Hz)")
 plt.title('freq Domain')
